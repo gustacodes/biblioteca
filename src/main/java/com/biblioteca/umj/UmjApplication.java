e4860045007904c818512d06f254f0e07aadb352
Gustavo Alencar <gustavorique@live.com>
2023-05-17 02:50:14 -0300

    Commit inicial

---
+++ src/main/java/com/biblioteca/umj/UmjApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.umj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmjApplication.class, args);
+	}
+
+}
