5c3d7c943a685fa8cffd27c31333d855644c3897
Gustavo Alencar <gustavorique@live.com>
2023-05-17 16:26:31 -0300

    Controller de livros atualizado com ResponseEntity

--- src/main/java/com/biblioteca/umj/controllers/LivroController.java
+++ src/main/java/com/biblioteca/umj/controllers/LivroController.java
@@ -3,6 +3,9 @@ package com.biblioteca.umj.controllers;
 import com.biblioteca.umj.entities.Livros;
 import com.biblioteca.umj.services.LivroServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,19 +18,19 @@ public class LivroController {
     LivroServices livroServices;
 
     @PostMapping("/cadastro")
-    public Livros save(@RequestBody Livros livro) {
-        return livroServices.save(livro);
+    public ResponseEntity<Livros> save(@RequestBody Livros livro) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(livroServices.save(livro));
     }
 
     @GetMapping
-    public List<Livros> findAll() {
-        return livroServices.findAll();
+    public ResponseEntity<List<Livros>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(livroServices.findAll());
     }
 
     @GetMapping("/{nome}")
-    public Livros findByNome(@PathVariable String nome) {
+    public ResponseEntity<Livros> findByNome(@PathVariable String nome) {
         Livros livro = livroServices.findByNome(nome);
-        return livro;
+        return ResponseEntity.status(HttpStatus.OK).body(livro);
     }
 
 }
